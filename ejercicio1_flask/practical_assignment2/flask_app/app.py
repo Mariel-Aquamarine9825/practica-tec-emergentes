cecad0de811f10942aaa6558603a552d285baba3
Mariel-Aquamarine9825 <71471367+Mariel-Aquamarine9825@users.noreply.github.com>
2022-09-30 12:39:18 -0400

    Add files via upload

---
+++ ejercicio1_flask/practical_assignment2/flask_app/app.py
@@ -0,0 +1,100 @@
+from flask import Flask, render_template,request,redirect
+from flask_mysqldb import MySQL
+
+import MySQLdb.cursors
+import json
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'dbcontainer'
+app.config['MYSQL_USER'] = 'example_users'
+app.config['MYSQL_PASSWORD'] = 'mysql'
+app.config['MYSQL_DB'] = 'example'
+#app.config['MYSQL_PORT'] = 3306
+
+mysql = MySQL(app)
+
+
+
+   
+@app.route('/', methods=['GET'])
+def professor_list_json():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM professor')
+    data = cursor.fetchall()
+    return json.dumps(data)
+@app.route("/home") 
+def  home(): 
+   return render_template("home.html") 
+
+@app.route("/nuevoprof")
+def nuevoprof():
+    return render_template("agregar_professor.html")
+def obtener():
+    professors= []
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT id,first_name,last_name,city,address,salary FROM professor')
+    professors = cursor.fetchall()
+    return professors 
+@app.route('/professors')
+def professors():
+    professors = obtener()
+    return render_template("profesores.html", professors=professors)
+    
+
+@app.route('/professorlist', methods=['GET'])
+def professor_list():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM professor')
+    data = cursor.fetchall()
+    return render_template('list.html', profesors=data)
+
+
+@app.route("/guardarprofessor", methods=["POST"])
+def guardar_professor():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    city = request.form["city"]
+    address = request.form["address"]
+    salary = request.form["salary"]
+    cursor .execute('INSERT INTO professor VALUES(NULL, % s, % s, % s, % s,%s)',(first_name, last_name,city,address,salary)) 
+    mysql.connection.commit()
+    return redirect("/professors")
+
+
+
+@app.route("/eliminar_profesor", methods=["POST"])
+def eliminar_profesor():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    id= request.form["id"]
+    cursor.execute("DELETE FROM professor WHERE id = %s", (id,))
+    mysql.connection.commit()
+    return redirect("/professors")
+def obtener_por_id(id):
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("SELECT id, first_name, last_name, city, address, salary precio FROM professor WHERE id = %s", (id,))
+    professors = cursor.fetchone()
+    return professors
+@app.route("/formulario_editar_prof/<int:id>")
+def editar_prof(id):
+    professors = obtener_por_id(id)
+    return render_template("editar.html",professors=professors)
+
+
+@app.route("/actualizar_professor", methods=["POST"])
+def actualizar_professor():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    id = request.form["id"]
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    city = request.form["city"]
+    address = request.form["address"]
+    salary = request.form["salary"]
+    cursor.execute("UPDATE professor SET first_name = %s, last_name = %s, city = %s, address = %s, salary = %s WHERE id = %s", (first_name, last_name, city, address, salary, id))
+    mysql.connection.commit()
+    return redirect("/professors")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=81, debug=True)
---
+++ ejercicio1_flask/practical_assignment2/php_app/test.php
@@ -0,0 +1,14 @@
+<?php
+    $conn = new mysqli("dbcontainer", "example_user", "mysql", "example", "3306");
+    $res = $conn->query("SELECT * FROM professor");
+    
+    while ($row = $res->fetch_assoc()):
+?>
+<div>
+    <?= $row["first_name"] ?>
+    <?= $row["last_name"] ?>
+</div>
+<?php
+    endwhile;
+    $conn->close();
+?>
